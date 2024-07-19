c5169b576967ad68647ee2891f9d516be4b0acab
muhrobby <robbymuh95@gmail.com>
2024-07-19 11:30:34 +0700

    feature helmet dan cors

--- src/app.js
+++ src/app.js
@@ -5,10 +5,35 @@ const { errorMiddleware } = require("./middleware/errorMiddleware");
 const logger = require("./config/logger");
 const { db, User } = require("./config/database");
 const authApi = require("./routes/authApi");
+const helmet = require("helmet");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubDomains"
+  );
+  res.setHeader("Content-Security-Policy", "default-src 'self'");
+  next();
+});
+
+app.use(express.json());
+app.use(publicApi);
+app.use(authApi);
+
+app.use(errorMiddleware);
 
 (async () => {
   try {
@@ -19,11 +44,5 @@ const app = express();
     logger.error("Unable to connect to the database:", error);
   }
 })();
-
-app.use(express.json());
-app.use(publicApi);
-app.use(authApi);
-
-app.use(errorMiddleware);
 
 module.exports = app;
